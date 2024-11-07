3157d98f8a4e575bf35016ab955b7e74dd79c8e4
mikealynch <mikelynch02115@gmail.com>
2024-11-06 20:40:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import streamlit as st
+
+# Define the containers for Streamlit layout
+header = st.container()
+data = st.container()
+features = st.container()
+modelTraining = st.container()
 
 
 
