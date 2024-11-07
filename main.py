94d99265d85714199fc700e9e8a10df517cf63b0
mikealynch <mikelynch02115@gmail.com>
2024-11-06 20:38:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,6 @@ from pandasai.llm import OpenAI
 import openai
 
 
-load_dotenv()
-api_key = os.getenv("API_KEY")
-
-openai.api_key = api_key
-
-
-openai_llm = OpenAI(api_token=openai.api_key)
 
 
 st.title("Let's analyze a CSV")
