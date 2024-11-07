29bbcee7a45f80e6ad0ae84efb0ab852f5e71637
mikealynch <mikelynch02115@gmail.com>
2024-11-06 20:42:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
+from dotenv import load_dotenv
+import os
 import streamlit as st
-
-# Define the containers for Streamlit layout
-header = st.container()
-data = st.container()
-features = st.container()
-modelTraining = st.container()
+import pandas as pd
+from pandasai import SmartDataframe
+from pandasai.llm import OpenAI
+import openai
 
 
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+openai.api_key = api_key
+
+
+openai_llm = OpenAI(api_token=openai.api_key)
 
 
 st.title("Let's analyze a CSV")
 
 uploaded_file = st.file_uploader("Upload a CSV file for analysis", type=['csv'])
 
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write("First three rows:")
+    st.write(df.head(3))
+    
+    prompt = st.text_area("Ask me a question about your file.")
+    
+    if st.button("GO"):
+        if prompt:
+            st.write("Let me think about that for a minute.")
+            
+            openai_llm = OpenAI(api_token=openai.api_key)
+
+            sdf=SmartDataframe(df, config={"llm": openai_llm})
+
+            ai_response = sdf.chat(prompt)
+            st.write(ai_response)
+
+        else:
+            st.warning("Ask me a question about your file.")
