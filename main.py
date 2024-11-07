a4b0a338e5335755740694ca5c527d3625f8262a
mikealynch <mikelynch02115@gmail.com>
2024-11-06 20:38:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ Created on Wed Nov  6 14:50:16 2024
 """
 
 
-from dotenv import load_dotenv
-import os
-import streamlit as st
-import pandas as pd
-from pandasai import SmartDataframe
-from pandasai.llm import OpenAI
-import openai
 
 
 
